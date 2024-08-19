7062e24aa9633fc0ed93ee3b474c8949b77e2d8b
Ayaan <syedayaan9376@gmail.com>
2024-08-19 21:45:45 +0530

    COMMITMENT OF ISSUES

--- app/src/main/java/eu/tutorials/parking_system/Login_Page.java
+++ app/src/main/java/eu/tutorials/parking_system/Login_Page.java
@@ -1,95 +1,98 @@
-package eu.tutorials.parking_system;
+    package eu.tutorials.parking_system;
+    
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.ProgressBar;
+    import android.widget.TextView;
+    import android.widget.Toast;
+    
+    import androidx.activity.EdgeToEdge;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
+    
+    import com.google.android.gms.tasks.OnCompleteListener;
+    import com.google.android.gms.tasks.Task;
+    import com.google.android.material.textfield.TextInputEditText;
+    import com.google.firebase.auth.AuthResult;
+    import com.google.firebase.auth.FirebaseAuth;
+    import com.google.firebase.auth.FirebaseUser;
+    
+    public class Login_Page extends AppCompatActivity {
+        TextInputEditText email, password;
+        Button btn_login;
+        FirebaseAuth mAuth;
+        ProgressBar bar;
+        TextView register;
+        @Override
+        public void onStart() {
+            super.onStart();
+    
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if(currentUser != null){
+                Intent intent=new Intent(Login_Page.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_login_page);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+            email = findViewById(R.id.email);
+            password = findViewById(R.id.password);
+            btn_login = findViewById(R.id.btn_login);
+            bar = findViewById(R.id.bar);
+            mAuth = FirebaseAuth.getInstance();
+            btn_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+    
+                    String email1, password1;
+                    email1 = String.valueOf(email.getText()).trim();
+                    password1 = String.valueOf(password.getText()).trim();
+                    if (email1.isEmpty() || password1.isEmpty()) {
+                        Toast.makeText(Login_Page.this, "Enter all fields", Toast.LENGTH_SHORT).show();
+                        bar.setVisibility(View.GONE);
+                        return;
+                    }
+                    bar.setVisibility(View.VISIBLE);
+                    btn_login.setVisibility(View.GONE);
+                    mAuth.signInWithEmailAndPassword(email1, password1)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+    //                                    Toast.makeText(Login.this, "Authentication Successfull",Toast.LENGTH_SHORT).show();
+                                        bar.setVisibility(View.GONE);
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class Login_Page extends AppCompatActivity {
-    TextInputEditText email, password;
-    Button btn_login;
-    FirebaseAuth mAuth;
-    ProgressBar bar;
-    TextView register;
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent=new Intent(Login_Page.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-            return;
+                                        Intent intent=new Intent(Login_Page.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                        return;
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(Login_Page.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        bar.setVisibility(View.GONE);
+                                        btn_login.setVisibility(View.VISIBLE);
+                                    }
+                                }
+                            });
+                }
+            });
         }
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        btn_login = findViewById(R.id.btn_login);
-        bar = findViewById(R.id.bar);
-        mAuth = FirebaseAuth.getInstance();
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String email1, password1;
-                email1 = String.valueOf(email.getText()).trim();
-                password1 = String.valueOf(password.getText()).trim();
-                if (email1.isEmpty() || password1.isEmpty()) {
-                    Toast.makeText(Login_Page.this, "Enter all fields", Toast.LENGTH_SHORT).show();
-                    bar.setVisibility(View.GONE);
-                    return;
-                }
-                bar.setVisibility(View.VISIBLE);
-                mAuth.signInWithEmailAndPassword(email1, password1)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
-//                                    Toast.makeText(Login.this, "Authentication Successfull",Toast.LENGTH_SHORT).show();
-                                    bar.setVisibility(View.GONE);
-                                    Intent intent=new Intent(Login_Page.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                    return;
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(Login_Page.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    bar.setVisibility(View.GONE);
-                                }
-                            }
-                        });
-            }
-        });
-    }
-}
